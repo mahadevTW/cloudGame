f62a384f1d8f21a4598d83333553fdc3cb38cd9a
mahadev <mahadev@thoughtworks.com>
2018-09-15 18:01:12 +0530

    First run

---
+++ src/main/java/com/example/couldgame/coudgame/CloudGameApplication.java
@@ -0,0 +1,11 @@
+package com.example.couldgame.coudgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGameApplication.class, args);
+    }
+}
